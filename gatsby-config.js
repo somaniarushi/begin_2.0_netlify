afa4dcd5e4087d3a8f6a5e0acad12540eea34321
Andrew Kirillov <kirillov.edu@gmail.com>
2020-04-25 01:02:15 -0400

    Revert "cleaned up filesystem, should build"
    This reverts commit c02bf3e83ba17f810843f364f674db9366e6811d.

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,34 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {
+              destinationDir: 'static',
+            },
+          },
+        ],
+      },
+    },
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
